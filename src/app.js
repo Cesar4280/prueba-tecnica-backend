4136fc7fa49816b647eddf7d43c964f8119a008d
Cesar Fajardo <cesarjavierfajardoortiz@gmail.com>
2024-02-21 12:34:17 -0500

    corriguiendo setencia INSERT INTO multiple en SQL

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import { typeDefs as deviceTypeDefs } from "./schema/deviceSchema.js";
 import projectResolvers from "./resolvers/projectResolvers.js";
 import deviceResolvers from "./resolvers/deviceResolvers.js";
 
+import "./models/projectModel.js";
 import "./models/deviceModel.js";
 
 testConnection(sequelize);
